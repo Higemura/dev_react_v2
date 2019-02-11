9db7583a91c76e11e462e4f5ad2fb531f8bb772c
Higemura <higemura.mustache@gmail.com>
2019-02-12 01:22:37 +0900

    JSX 構文について記述

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+// JSXを使用する場合はimport Reactが必須（使わなければ必要ない）
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    // render内に変数を記述できる
+    const text = "Hi, Tom";
+    // JSXのclassはclassNameにしなければならない
+    const dom = <h1 className="foo">{text}</h1>;
     return (
-      <div>
-        <h1> Hello World!</h1>
-      </div>
+      // returnの中は一つのタグの中に囲まれていないといけない。<div>などで囲むでも良いが<React.Fragment>で囲むと不要な親タグが出力されないで描画できる
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am Clicked.")}} />
+        {/* dom変数を出力させる（return内が複数行の場合は{}で変数を囲まないといけない）*/}
+        {dom}
+      </React.Fragment>
     );
   }
 }
