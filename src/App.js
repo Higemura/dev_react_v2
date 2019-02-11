cd09781f22b7f2a08c0ce44f96f71843015eadb4
Higemura <higemura.mustache@gmail.com>
2019-02-12 03:35:25 +0900

    stateはコンポーネント内で使用できる。値の変更可能

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
 // クラスコンポーネントを使わなければ { Component } はいらない（使われてないのに指定しているとエラーが出る）
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 // const App は関数コンポーネント
-const App = () => {
-  const profiles = [
-    {
-      name: "Taro",
-      age: 10
-    },
-    {
-      name: "Hanakp",
-      age: 5
-    },
-    {
-      name: "NoName"
-      // ageが設定されてないのでPropsのデフォルト値からageを取得させる
-    }
-  ];
-  return (
-    <React.Fragment>
-      {
-        // mapでprofilesの配列をループさせる（第一引数にprofileを指定してprofile.nameのように設置する）
-        // keyにユニークキーを設定することによってVDOMの変更箇所を教えてあげる（設定してないとWarning Errorが出る）
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-      {/* <User name={"Taro"} age={10} />
-      <User name={"Hanako"} age={5} /> */}
-    </React.Fragment>
-  )
-}
+const App = () => (<Counter></Counter>)
 
 // propsでnameとageの値を渡す
-const User = (props) => {
-  return <div>Hi, I am {props.name}!, and {props.age} yers old!</div>
-}
+class Counter extends Component {
+  // コンストラクターはpropsを受け取ることができる
+  constructor(props) {
+    // super内にpropsを指定して初期化する
+    super(props)
+    // stateの中身を確認（undefined）
+    console.log(this.state)
+    // stateにcountを追加する
+    this.state = {count: 0}
+  }
 
-// Propsのデフォルト値を設定（Propsに値がなかった時などはここから取得させる）
-// User.defaultProps = {
-//   age: 1
-// }
+  // プラスボタン
+  handlePlusButton = () => {
+    console.log('プラスボタン クリック');
+    console.log(this.state.count);
 
-// propsの型を指定する
-User.propTypes = {
-  // stringで文字列にできる（nameに数字などが入るとエラーを出す）
-  name: PropTypes.string,
-  // numberで数字にできる（ageに文字列などが入るとエラーを出す）
-  age: PropTypes.number.isRequired // isRequiredでageを必須に設定できる（ageが存在しないとWarning Errorが出る）
+    // setStateでstateに状態（値）をセットできる
+    this.setState({ count: this.state.count + 1})
+
+    // setStateを使わずthis.stateを直接更新することはできない。stateの値を変えるには必ずsetStateを使う。
+    // this.state = {count: this.state.count + 1}
+  }
+
+  // マイナスボタン
+  handleMinusButton = () => {
+    console.log('マイナスボタン クリック');
+    console.log(this.state.count);
+
+    // setStateでstateに状態（値）をセットできる
+    this.setState({ count: this.state.count - 1})
+
+    // setStateを使わずthis.stateを直接更新することはできない。stateの値を変えるには必ずsetStateを使う。
+    // this.state = {count: this.state.count - 1}
+  }
+
+  render() {
+    // stateの中身を確認（this.stateに何も設定してないとnullになる）
+    console.log(this.state)
+    // this.stateからcountを取得する
+    return (
+      <React.Fragment>
+        <div>counter: { this.state.count }</div>
+        <button onClick={ this.handlePlusButton }>+1</button>
+        <button onClick={ this.handleMinusButton }>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
