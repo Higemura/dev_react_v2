2b27d0df64f88c6aac8ec1f0acca300d976b1aa1
Higemura <higemura.mustache@gmail.com>
2019-02-12 02:28:17 +0900

    .mapでpropsをループ処理させる。defaultPropsでpropsの初期値を設定できる。

--- src/App.js
+++ src/App.js
@@ -5,40 +5,45 @@
 // クラスコンポーネントを使わなければ { Component } はいらない（使われてないのに指定しているとエラーが出る）
 import React from 'react';
 
-
-// class App のことをクラスコンポーネントと言う
-// class App extends Component {
-//   render() {
-//     // render内に変数を記述できる
-//     const text = "Hi, Tom";
-//     // JSXのclassはclassNameにしなければならない
-//     const dom = <h1 className="foo">{text}</h1>;
-//     return (
-//       // returnの中は一つのタグの中に囲まれていないといけない。<div>などで囲むでも良いが<React.Fragment>で囲むと不要な親タグが出力されないで描画できる
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am Clicked.")}} />
-//         {/* dom変数を出力させる（return内が複数行の場合は{}で変数を囲まないといけない）*/}
-//         {dom}
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 // const App は関数コンポーネント
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10
+    },
+    {
+      name: "Hanakp",
+      age: 5
+    },
+    {
+      name: "NoName"
+      // ageが設定されてないのでPropsのデフォルト値からageを取得させる
+    }
+  ];
   return (
     <React.Fragment>
-      {/* const Catを呼び出す */}
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        // mapでprofilesの配列をループさせる（第一引数にprofileを指定してprofile.nameのように設置する）
+        // keyにユニークキーを設定することによってVDOMの変更箇所を教えてあげる（設定してないとWarning Errorが出る）
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+      {/* <User name={"Taro"} age={10} />
+      <User name={"Hanako"} age={5} /> */}
     </React.Fragment>
   )
 }
 
-const Cat = () => {
-  return <div>にゃおにゃお！</div>
+// propsでnameとageの値を渡す
+const User = (props) => {
+  return <div>Hi, I am {props.name}!, and {props.age} yers old!</div>
+}
+
+// Propsのデフォルト値を設定（Propsに値がなかった時などはここから取得させる）
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
