25ac78d6e4af998ee487cbdad2c53119fa01f0ea
Higemura <higemura.mustache@gmail.com>
2019-02-12 00:58:44 +0900

    JSXとは何か

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
+
+// JSXでの書き方。構文が少なくて済む（直感的にコーディングできる）
+// class App extends Component {
+//   render() {
+//     // この部分
+//     return (
+//       <div>
+//         Hello World!
+//       </div>
+//     );
+//   }
+// }
+
+// JavaScriptでの書き方。return の後にReact.createElement内に引数として記述する
+// class App extends Component {
+//   render() {
+//     // この部分
+//     return React.createElement(
+//       "div", // 第一引数
+//       null, // 第二引数
+//       "Hello World!!!" // 第三引数
+//     );
+//   }
+// }
+
+
+// 練習
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1> Hello World!</h1>
-      </div>
-    );
+    // returnの後に()がなければ一文で記述する
+    return <div><h1>Hello World!</h1></div>;
   }
 }
 
